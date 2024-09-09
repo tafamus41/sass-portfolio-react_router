43be1aba28223b09c219243090dcb3b24c0b11ec
asimov <seyranmustafa4@gmail.com>
2024-09-09 13:03:34 +0300

    p

--- src/components/Navbar/MyNavbar.jsx
+++ src/components/Navbar/MyNavbar.jsx
@@ -1,18 +1,27 @@
 import { Container } from "react-bootstrap"
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import {NavLink } from 'react-router-dom';
 
 const MyNavbar = () => {
   return (
     <div>
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
+              className="nav-link"
+              to="/">HOME</NavLink>
+            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
+              className="nav-link"
+              to="/about">ABOUT ME</NavLink>
+            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
+              className="nav-link"
+              to="/projets">MY PROJECTS</NavLink>
+            <NavLink style={({ isActive }) => ({ color: isActive && "red" })}
+              className="nav-link"
+              to="/contact">CONTACT</NavLink>
           </Nav>
         </Container>
       </Navbar>
