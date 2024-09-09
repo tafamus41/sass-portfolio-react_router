71f1a31bb19f6c820ac399c69a0630e376da5ffd
asimov <seyranmustafa4@gmail.com>
2024-09-09 15:53:13 +0300

    p

--- src/components/Navbar/MyNavbar.jsx
+++ src/components/Navbar/MyNavbar.jsx
@@ -1,31 +1,51 @@
-import { Container } from "react-bootstrap"
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {NavLink } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 const MyNavbar = () => {
   return (
     <div>
-        <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar expand="lg" bg="light">
         <Container>
-          <Nav className="me-auto">
-            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
-              className="nav-link"
-              to="/">HOME</NavLink>
-            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
-              className="nav-link"
-              to="/about">ABOUT ME</NavLink>
-            <NavLink style={({ isActive }) => ({ color: isActive && "orange" })}
-              className="nav-link"
-              to="/projets">MY PROJECTS</NavLink>
-            <NavLink style={({ isActive }) => ({ color: isActive && "red" })}
-              className="nav-link"
-              to="/contact">CONTACT</NavLink>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && "orange" })}
+                className="nav-link"
+                to="/"
+              >
+                HOME
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && "orange" })}
+                className="nav-link"
+                to="/about"
+              >
+                ABOUT ME
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && "orange" })}
+                className="nav-link"
+                to="/projets"
+              >
+                MY PROJECTS
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && "red" })}
+                className="nav-link"
+                to="/contact"
+              >
+                CONTACT
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
-  )
-}
+  );
+};
 
 export default MyNavbar;
