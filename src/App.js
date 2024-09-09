8403e38128182321247d66bc879f794281fd0147
asimov <seyranmustafa4@gmail.com>
2024-09-09 12:20:41 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import React from 'react'
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import AppRouter from "./router/AppRouter"
+import Router from "./router/Router";
 
-// import {BrowserRouter, Route, Routes} from "react-router-dom"
-// import Home from "./pages/Home";
-// import MyNavbar from './components/MyNavbar';
-// import Teacher from './pages/Teacher';
-// import CourseCard from './pages/CourseCard';
-// import ContactForm from './pages/ContactForm';
-// import Paths from './pages/Paths';
+
 const App = () => {
   return (
     <div>
-   
-<AppRouter/>
-
-{/* <BrowserRouter>
-
-<MyNavbar/>
-
-
-<Routes>
-<Route path="/" element={<Home/>} />
-<Route path="/teacher" element={<Teacher/>} />
-<Route path="/courses" element={<CourseCard/>} />
-<Route path="/contact" element={<ContactForm/>} />
-<Route path="/paths" element={<Paths/>} />
-
-
-</Routes>
-
-
-</BrowserRouter> */}
-
-
+      <Router />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
