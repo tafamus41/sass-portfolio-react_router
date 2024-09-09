b365d731e9190c6f4af373f00c14762ed96326d5
asimov <seyranmustafa4@gmail.com>
2024-09-09 17:10:06 +0300

    p

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='homePage'>
         <p>Hi!My Name Is</p>
         <h1>Wednesday<span>Addams</span></h1>
-        <hr style={{ border: '2px solid white', width: '100%' }}/>
+        <hr/>
         <p>Full Stack Developer</p>
     </div>
   )
