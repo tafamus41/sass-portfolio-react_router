03aec70d8964e0bed8757644fcf4da7a477c5fc8
asimov <seyranmustafa4@gmail.com>
2024-09-09 12:36:37 +0300

    p

---
+++ src/router/Router.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Projects from "../pages/projects/Projects";
+import Contact from "../pages/contact/Contact";
+
+const Router = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Router;
