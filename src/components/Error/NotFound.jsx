34997cce21734fa318ebfc4bea40225d59956b9a
asimov <seyranmustafa4@gmail.com>
2024-09-10 07:11:07 +0300

    p

---
+++ src/components/Error/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import notFound from "../../assets/img/notFound.jpeg"
+
+const NotFound = () => {
+    const navigateee =useNavigate()
+
+  return (
+    <Container className="text-center notFound">
+      <img src={notFound} alt="" width="550px" />
+
+     
+        <button onClick={()=>navigateee("/")} className="btn btn-info">GO HOME</button>
+    
+    </Container>
+  )
+}
+
+export default NotFound
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,6 +6,7 @@ import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 import Projects from "../pages/projects/Projects";
 import Contact from "../pages/contact/Contact";
+import NotFound from "../components/Error/NotFound";
 
 const Router = () => {
   return (
@@ -17,6 +18,7 @@ const Router = () => {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
